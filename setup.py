5221b70486e1afab390d4a51cf82941a88888b4f
Thom van der Woude <tbvanderwoude@gmail.com>
2021-08-23 19:50:37 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="mapf-branch-and-bound",
-    version="0.0.1",
+    version="0.0.2",
     author="Thom van der Woude",
     author_email="tbvanderwoude@student.tudelft.nl",
     description="A branch-and-bound framework for MAPF solvers that use mapfmclient",
